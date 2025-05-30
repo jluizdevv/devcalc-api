401970900c4426ac0a2c387a193774be2b7c4be6
José <jluizbc94@gmail.com>
2025-05-30 13:46:04 -0300

    Adiciona arquivos que estavam só na feature/calculator-endpoints

---
+++ src/main/java/devcalc/controller/CalculatorController.java
@@ -0,0 +1,35 @@
+package devcalc.controller;
+
+import devcalc.service.CalculatorService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class CalculatorController {
+
+    private final CalculatorService service;
+
+    public CalculatorController(CalculatorService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return service.add(a, b);
+    }
+
+    @GetMapping("/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return service.subtract(a, b);
+    }
+
+    @GetMapping("/multiply")
+    public int multiply(@RequestParam int a, @RequestParam int b) {
+        return service.multiply(a, b);
+    }
+
+    @GetMapping("/divide")
+    public double divide(@RequestParam int a, @RequestParam int b) {
+        return service.divide(a, b);
+    }
+}
